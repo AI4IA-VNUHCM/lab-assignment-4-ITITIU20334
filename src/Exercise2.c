f5b6b50fe42f855ff3e75bd0bf83e0e63881d916
ITITIU20334 <91869183+ITITIU20334@users.noreply.github.com>
2021-10-25 19:12:55 +0700

    ex245

--- src/Exercise2.c
+++ src/Exercise2.c
@@ -22,60 +22,65 @@ ________________________________________________________________________________
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <stdbool.h>
+#define SIZE 100
+
+void Array2Dconverter(int arr[], int a[SIZE][SIZE], int m, int n)
+{
+	int row, column;
+	int counter = 0;
+	//Convert 1D array to 2D array
+	for (row = 0; row <= (m - 1); row ++){
+		for (column = 0; column <= (n - 1); column ++){
+			a[row][column] = arr[counter];
+			counter++;
+		}
+	}
+}
+
+void printArray(int a[SIZE][SIZE], int m, int n)
+{
+	int row, column;
+
+	for (row = 0; row <= (m - 1); row ++){
+		for (column = 0; column <= (n - 1); column ++){
+			printf("%d ", a[row][column]);
+		}
+		printf("\n");
+	}
+}
+
+void Ex2(int arr[], int m, int n){
+	int a[SIZE][SIZE];
+	Array2Dconverter(arr,a,m,n);
+	//Your codes here
+int column, row;
+	for(column = 0; column < n; column++)
+	{
+	for(row = 0; row < m; row++)
+	{
+	for(int sort = row + 1; sort < m; sort++){
+	if((a[row][column] - a[sort][column]) * ((column % 2 + 1) * 2 - 3) > 0){
+	int temp = a[row][column];
+	a[row][column] = a[sort][column];
+	a[sort][column] = temp;
+	}
+	}
+	}
+	}
+	printArray(a, m, n);
+}
 
 int main(int argc, char *argv[]) {
-
+	//testing variable, applying it to your algorithm for auto-evaluating
 	int row = atoi(argv[1]);
 	int col = atoi(argv[2]);
 	argc-=3;
-	int rows[argc/col][argc/col];
-    int i = 3;
-
-    
-    for(int r = 0; r < row; r++){
-		for(int c = 0; c < col; c++){
-            rows[r][c] = atoi(argv[i]);
-            i++;
-        }
+	int testcase[argc],i;
+	for(i=0; i<argc;i++){
+		testcase[i] = atoi(argv[i+3]);
 	}
- int current, next, temp;
-    for(int i = 0; i < row; i ++){
-        for(int r = 0; r < row - 1; r++){
-            for(int c = 0; c < col; c++){
-                current = rows[r][c];
-                next = rows[r+1][c];
-                
-                if(c%2 == 0){
-                    
-                    if(next > current)
-                    {
-                        temp = rows[r][c];
-                        rows[r][c] = next;
-                        rows[r+1][c] = temp;
-                    }
-                }else{
-                    
-                    if(next < current)
-                    {
-                        temp = rows[r][c];
-                        rows[r][c] = next;
-                        rows[r+1][c] = temp;
-                    }
-                }
-                
-            }
-        }
-    }
 
-    for(int r = 0; r < row; r++){
-		for(int c = 0; c < col; c++){
-            printf("%d ", rows[r][c]);
-        }
-        printf("\n");
-	}
+	Ex2(testcase, row, col);
 	
-    printf("\n");
 	return 0;
 }
